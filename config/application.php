36577b3239614bd09da9ec00496c7bfc5ef6b453
Mike <mikeh@ioprint.com>
2015-12-18 16:38:41 -0500

    sunrise on

--- config/application.php
+++ config/application.php
@@ -75,6 +75,7 @@ define('NONCE_SALT', getenv('NONCE_SALT'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', getenv('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_EDIT', true);
+define( 'SUNRISE', 'on' );
 
 /**
  * Bootstrap WordPress
